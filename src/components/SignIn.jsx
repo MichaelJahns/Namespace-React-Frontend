32c4452242f4bbd5d11b72cf06a37bf40cbaffef
MichaelJahns <jahnsmic001@gmail.com>
2019-11-20 17:29:19 -0800

    Reselects base color and backgroundcolor for improved visibility. We went from paper white and gray to paper white and brown

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -5,44 +5,40 @@ import '../resources/css/signin.css';
 export default class signIn extends Component {
     _getClassNames = () => {
         let signShown = this.props.signShown;
-        return signShown;
+        return "signIn " + signShown;
     };
 
     render() {
-        // TODO: State that tells this component to display as either a sign in or a sign up form
         return (
             <div className={this._getClassNames()}>
-                <div className="blurPlate">x</div>
-                <div className="signIn">
-                    <div>
-                        <h2> Sign In </h2>
-                        {/* TODO: add the ability to minimize the sign in component on the component */}
-                    </div>
+                <div>
+                    <h2> Sign In </h2>
+                    {/* TODO: add the ability to minimize the sign in component on the component */}
+                </div>
 
-                    <form>
-                        <p>
-                            <input placeholder="Username or e-mail.." type="text" />
-                            {/* TODO: how to do two in one? */}
-                            {/* <label>Username or Email Address</label> */}
-                            <label> Username </label>
-                        </p>
-                        <p>
-                            <input placeholder="Password.." type="password" />
-                            <label> Password</label>
-                        </p>
-                        <p className="tester">
-                            <label htmlFor="remember-me">
-                                Remember me?
+                <form>
+                    <p>
+                        <input placeholder="Username or e-mail.." type="text" />
+                        {/* TODO: how to do two in one? */}
+                        {/* <label>Username or Email Address</label> */}
+                        <label> Username </label>
+                    </p>
+                    <p>
+                        <input placeholder="Password.." type="password" />
+                        <label> Password</label>
+                    </p>
+                    <p className="tester">
+                        <label htmlFor="remember-me">
+                            Remember me?
                         </label>
-                            <input id="remember-me" type="checkbox" name="remember-me" />
+                        <input id="remember-me" type="checkbox" name="remember-me" />
 
-                        </p>
-                        <button>
-                            Sign in
+                    </p>
+                    <button>
+                        Sign in
                     </button>
-                    </form>
-                </div >
-            </div>
+                </form>
+            </div >
 
         )
     }
