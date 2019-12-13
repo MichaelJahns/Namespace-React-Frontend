758724463cc6557413ae04152b92ae2a94a173b8
MichaelJahns <jahnsmic001@gmail.com>
2019-12-13 14:14:34 -0800

    Main now handles authenticated viewing, restructures hierachy to create View, which contains the three visible sections, nav, main, and footer

--- src/utils/functionalComponent/Dashboard.js
+++ src/utils/functionalComponent/Dashboard.js
@@ -1,24 +1,13 @@
 import React from "react";
-import { useAuth } from '../useAuth';
 import CharacterView from './CharacterView';
 import '../../resources/css/dashboard.css'
 
 function Dashboard() {
-    const firebase = useAuth();
 
     return (
-        <React.Fragment>
-            {firebase.user ?
-                < div className="content" >
-                    <CharacterView />
-                </div >
-                :
-                <div className="content">
-                    <p> Please Login </p>
-                </div>
-            }
-
-        </React.Fragment>
+        <div className="dashboard">
+            <CharacterView />
+        </div>
     );
 }
 
