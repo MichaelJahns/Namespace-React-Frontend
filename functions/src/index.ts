11129046b76d710b785ed809fef252e6f8e29ec5
MichaelJahns <jahnsmic001@gmail.com>
2020-01-08 09:48:49 -0800

    Writes first cloud function, experimenting with giving each navigation button an alternate look

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,26 @@
 import * as functions from 'firebase-functions';
+import { database } from '../util/admin';
+
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});
+
+export const getAllCharacters = functions.https.onRequest((request, response) => {
+    database
+        .collection("characters")
+        .where("campaign", "==", "iqNOydMMd4hJY5uxmveW")
+        .get()
+        .then((querySnapshot: any) => {
+            const characters: any[] = []
+            querySnapshot.forEach((doc: any) => {
+                characters.push(doc.data);
+            })
+            return response.json(characters);
+        })
+        .catch((err: any) => console.error(err))
+})
+
---
+++ functions/util/admin.ts
@@ -0,0 +1,6 @@
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+const database = admin.firestore();
+
+export { admin, database };
